b3d0a95e8ce00234d3c3ae8a9b3aca23ecc40ec8
TWITexe <kawaii_senpaii@mail.ru>
2026-03-20 05:20:19 +0700

    feat(script) add level manager a.k level transition script

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    [SerializeField] private Color requiredColor = Color.white; // требуемый цвет дл€ перехода на некст уровень
+    [SerializeField] int sceneNumber;
+    [SerializeField] float timeToFadeStartLevelTransition; // врем€ дл€ Fade вначале уровн€
+    [SerializeField] float timeToFadeNextLevelTransition;  // врем€ дл€ Fade при переходе на новый уровень
+    
+
+    private void Start()
+    {
+        if (FadeScreen.instance != null)
+            FadeScreen.instance.FadeOut(timeToFadeNextLevelTransition);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        BrushColorController brushColor = collision.gameObject.GetComponent<BrushColorController>();
+        Debug.Log(collision.gameObject.GetComponent<BrushColorController>());
+        if (brushColor == null) return;
+
+        // сравнение двух цветов, если они очень похожи или идентичны - проходит!
+        float difference = Vector3.Distance(
+            new Vector3(brushColor.CurrentColor.r, brushColor.CurrentColor.g, brushColor.CurrentColor.b),
+            new Vector3(requiredColor.r, requiredColor.g, requiredColor.b));
+
+        if (difference < 0.1f)
+        {
+            Debug.Log("ќтлично! Ќовый уровень)");
+
+            if(FadeScreen.instance != null)
+                FadeScreen.instance.FadeIn(timeToFadeStartLevelTransition, () => { SceneManager.LoadScene(sceneNumber); });
+        }
+    }
+
+}
