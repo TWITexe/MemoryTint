ad7578180f19c2713d015c3c776513e91df402fd
Dxnil <ponkratenko.danil@mail.ru>
2026-03-22 15:21:08 +0300

    feat(script): add platform marker setup
    - Add PlatformMarker to batch-mark child sprites as platforms
    - Assign Ground layer/material and sync PaintSurface color from SpriteRenderer
    - Include Unity .meta files for the new platform scripts folder

---
+++ Assets/Scripts/Platform/PlatformMarker.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+
+[DisallowMultipleComponent]
+public class PlatformMarker : MonoBehaviour
+{
+    [SerializeField] private Transform platformsRoot;
+    [SerializeField] private Material platformMaterial;
+    [SerializeField] private bool syncSurfaceColorWithSprite = true;
+
+    [ContextMenu("Mark Platforms")]
+    public void MarkPlatforms()
+    {
+        if (platformsRoot == null)
+        {
+            Debug.LogWarning("PlatformMarker: platformsRoot is not assigned.", this);
+            return;
+        }
+
+        int groundLayer = LayerMask.NameToLayer("Ground");
+        int markedCount = 0;
+
+        Transform[] allTransforms = platformsRoot.GetComponentsInChildren<Transform>(true);
+
+        for (int i = 0; i < allTransforms.Length; i++)
+        {
+            Transform target = allTransforms[i];
+            if (target == platformsRoot)
+            {
+                continue;
+            }
+
+            SpriteRenderer spriteRenderer = target.GetComponent<SpriteRenderer>();
+            if (spriteRenderer == null)
+            {
+                continue;
+            }
+
+            if (groundLayer >= 0)
+            {
+                target.gameObject.layer = groundLayer;
+            }
+
+            if (platformMaterial != null)
+            {
+                spriteRenderer.sharedMaterial = platformMaterial;
+            }
+
+            PaintSurface paintSurface = target.GetComponent<PaintSurface>();
+            bool createdNow = false;
+
+            if (paintSurface == null)
+            {
+                paintSurface = target.gameObject.AddComponent<PaintSurface>();
+                createdNow = true;
+            }
+
+            if (paintSurface != null && (syncSurfaceColorWithSprite || createdNow))
+            {
+                paintSurface.SetSurfaceColor(spriteRenderer.color);
+            }
+
+            markedCount++;
+        }
+
+#if UNITY_EDITOR
+        EditorSceneManager.MarkSceneDirty(gameObject.scene);
+#endif
+
+        Debug.Log($"PlatformMarker: marked {markedCount} platform objects.", this);
+    }
+}
