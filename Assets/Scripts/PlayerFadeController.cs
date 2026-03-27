c4640e513a29e2fb9fbc35b49ad6d35e8ceb14fd
TWITexe <kawaii_senpaii@mail.ru>
2026-03-27 07:34:55 +0700

    feat(script) player fade mechanic for a smooth character appearance

---
+++ Assets/Scripts/PlayerFadeController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerFadeController : MonoBehaviour
+{
+    [SerializeField] private PlayerMoveController playerMoveController;
+    [SerializeField] private SpriteRenderer[] renderers;
+    [SerializeField] private float fadePlayerDuration = 2f;
+
+    private Coroutine currentFade;
+
+    private void Start()
+    {
+        SetAlpha(0f);
+    }
+
+    public void FadeIn(float duration = -1f)
+    {
+        float finalDuration = duration > 0f ? duration : fadePlayerDuration;
+        StartFade(0f, 1f, finalDuration, true);
+    }
+
+    public void FadeOut(System.Action onComplete = null, float duration = -1f)
+    {
+        float finalDuration = duration > 0f ? duration : fadePlayerDuration;
+        StartFade(1f, 0f, finalDuration, false, onComplete);
+    }
+
+    public void SetVisibleImmediate(float alpha)
+    {
+        SetAlpha(alpha);
+    }
+
+    private void StartFade(float from, float to, float duration, bool unlockAfter, System.Action onComplete = null)
+    {
+        if (currentFade != null)
+            StopCoroutine(currentFade);
+
+        currentFade = StartCoroutine(FadeRoutine(from, to, duration, unlockAfter, onComplete));
+    }
+
+    private IEnumerator FadeRoutine(float from, float to, float duration, bool unlockAfter, System.Action onComplete)
+    {
+        playerMoveController?.LockMove();
+
+        float time = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float t = Mathf.Clamp01(time / duration);
+            float alpha = Mathf.Lerp(from, to, t);
+            SetAlpha(alpha);
+            yield return null;
+        }
+
+        SetAlpha(to);
+
+        if (unlockAfter)
+            playerMoveController?.UnlockMove();
+
+        onComplete?.Invoke();
+    }
+
+    private void SetAlpha(float alpha)
+    {
+        foreach (var r in renderers)
+        {
+            if (r == null) continue;
+
+            Color c = r.color;
+            c.a = alpha;
+            r.color = c;
+        }
+    }
+}
